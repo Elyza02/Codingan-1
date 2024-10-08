920b500e73492e72d5fb329edc01c96667d391d7
Elyza <elyzazairin@gmail.com>
2024-10-08 11:43:40 +0700

    Coding

---
+++ pemograman 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//merdeklarasikan sebuah variabel integar
+	int angka;
+	double pecahan;
+	char jenisKelamin;
+	string kalimat;
+	float pecahanKecil;
+	
+	//inisialasi tipe data
+	angka=100,
+	pecahan=3.200;
+	jenisKelamin='p';
+	kalimat="elyza";
+	pecahanKecil=3,14;
+	
+	cout<< angka << endl;
+	cout<< pecahan << endl;
+	cout<< jenisKelamin << endl;
+	cout<< kalimat << endl;
+	cout<< pecahanKecil << endl;
+	
+	return 0;
+}
